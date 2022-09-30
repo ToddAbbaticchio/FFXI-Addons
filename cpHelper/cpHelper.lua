30463bd001dfe8bc6ea45c6931d5acd6916fa3f9
Shinyhelmet <86276724+helmbrechtjohn@users.noreply.github.com>
2022-09-30 10:03:20 -0500

    Conquest Point Update
    Updated to actually take your Conquest Point Amount into consideration on when to stop buying stuff.   This will repeat until the repeated amount will exceed total CP then quit.

--- cpHelper/cpHelper.lua
+++ cpHelper/cpHelper.lua
@@ -3,20 +3,41 @@ _addon.author = 'Risca'
 _addon.version = '0.0.1'
 _addon.commands = {'cphelper','ch'}
 
+--
+require('lists')
+require('logger')
 packets = require('packets')
+--
+
+
+-- Currency needed variables 
+local player_name = windower.ffxi.get_info().logged_in and windower.ffxi.get_player().name
+local check = {}
+packets2= require('packets')
+
+local requests = {
+    [0x113] = packets2.new('outgoing', 0x10F),
+    [0x118] = packets2.new('outgoing', 0x115),
+}
+
 
 nearNPC = false
+repeatamount = 0
 
 debug = false
 chatColor = 050
 refPacket = {}
 buyingCpItems = false
-CpNpcZones = T{"Windurst Waters", "Port Windurst", "Windurst Woods"}
+CpNpcZones = T{"Windurst Waters", "Port Windurst","Windurst Woods"}
 CpNpcZones = {
 	[238] = {npc="Puroiko-Maiko, W.W.", menu=32759, option=32800, unknown1=0}, --Windurst Waters
 	[240] = {npc="Milma-Hapilma, W.W.", menu=32759, option=32800, unknown1=0}, --Port Windurst
 	[241] = {npc="Harara, W.W.", menu=32759, option=32800, unknown1=0}, -- Windurst Woods
 }
+
+
+WindyCurrencyType = T{"Conquest Points (Windurst)"}
+currentCurrencyValue = 0
 
 function startTransaction(target,targetIndex)
 	writeLog(debug, chatColor, 'starting startTransaction()')
@@ -146,9 +167,12 @@ windower.register_event('addon command', function(...)
 	local quantity = 1
 	if arg[2] ~= nil then
 		quantity = arg[2]
+		repeatamount = arg[2]
 	end
 	if arg[3] ~= nil then
 		nearNPC = true
+	else 
+		nearNPC = false
 	end
 
 	if action:lower() == 'buy' then
@@ -161,9 +185,10 @@ windower.register_event('addon command', function(...)
 			checkState()
 		end
 		writeLog(true, chatColor, '-- CPHELPER VICTORIOUS!!! --')
+		coroutine.sleep(1)
 		windower.send_command('input //sellnpc mrc.cpt. scythe')
 		
-		--this works near Baehu-Faehu as long as windy owns Sarutabaruta  *highly likely* run away when done 
+		--this works near Baehu-Faehu as long as windy owns Sarutabaruta  *highly likely*
 		if nearNPC == true then
 			windower.send_command('input /targetnpc')
 		
@@ -175,8 +200,8 @@ windower.register_event('addon command', function(...)
 			end
 			coroutine.sleep(3)
 		
-			--lets go again....   
-			windower.send_command('input //ch buy ' ..arg[2].. ' '..arg[3])
+			--lets go again....   until we can't
+			findcurrency(WindyCurrencyType)
 		end
 		
 	end
@@ -197,6 +222,14 @@ windower.register_event('addon command', function(...)
 	end
 end)
 
+
+function pressKey(theKey, pauseDuration)
+    windower.send_command('setkey ' .. theKey .. ' down')
+    coroutine.sleep(pauseDuration)
+    windower.send_command('setkey ' .. theKey .. ' up')
+    coroutine.sleep(pauseDuration)
+end
+
 windower.register_event('incoming chunk',function(id,data,modified,injected,blocked)
 	if (id == 0x034 or id == 0x032) and buyingCpItems and refPacket then
 		writeLog(debug, chatColor, 'refPacket: '..tostring(refPacket))
@@ -209,6 +242,98 @@ windower.register_event('incoming chunk',function(id,data,modified,injected,bloc
 		return true
 	end
 end)
+
+
+--finding curency info -   
+function findcurrency(currency)
+    term = currency:concat(' ')
+
+    if not search_fields(term) then
+        error('No currencies matching: %s':format(term))
+        return
+    end
+end
+
+
+function comma_value(n) -- credit http://richard.warburton.it
+    local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
+    return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
+end
+
+windower.register_event('incoming chunk', function(id, data, amount, flag)
+    if check[id] then
+        local packet = packets.parse('incoming', data)
+
+        for field in check[id]:it() do
+            local msg = '%s: %s (%s)':format(player_name, field, comma_value(packet[field]))
+			currentCurrencyValue = packet[field]
+
+            writeLog(true, chatColor, msg)
+			writeLog(true, chatColor, currentCurrencyValue)
+			
+			--hard coded for merc captain scythe amount.    will change later if needed 
+			if currentCurrencyValue > (repeatamount * 4000) then 
+				windower.send_command('input //ch buy ' ..repeatamount.. ' true')
+			else 
+				nearNPC = false
+				writeLog(true, chatColor, '--You are done sir, good day.--')
+				writeLog(true, chatColor, '--I SAID GOOD DAY SIR!-- ')
+			end
+			
+        end
+
+        check[id] = nil
+    end
+end)
+
+function search_fields(terms)
+
+	--if ever need to expand this to a pipe delimited list for the other nations this is here... useless now. 
+    terms = terms:split('|'):map(string.gsub-{'[%s%p]', '.*'} .. string.lower)
+
+    if windower.ffxi.get_info().logged_in then
+        check = {}
+        local results
+
+        for id, packet in pairs(requests) do
+            local fields = packets2.fields('incoming', id)
+
+            for field in fields:it() do
+                if field.type ~= 'data' then
+                    local str = field.label:gsub('[%s%p]', ''):lower()
+
+                    for term in terms:it() do
+                        if str:find(term) then
+                            check[id] = check[id] or L{}
+                            check[id]:append(field.label)
+                        end
+                    end
+                end
+            end
+
+            if check[id] then
+                packets2.inject(packet)
+                results = true
+            end
+        end
+        return results
+    end
+end
+
+
+function findcurrency(currency)
+    local term = currency:concat(' ')
+
+    if not search_fields(term) then
+        error('No currencies matching: %s':format(term))
+        return
+    end
+end
+
+windower.register_event('login', function(name)
+    player_name = name
+end)
+
 
 -- don't need this garbage anymore.
 --[[ windower.register_event('outgoing chunk', function(id, data, modified, injected, blocked)
